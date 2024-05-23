c6dffb567d2434af05dcdde242190a78eab29e87
naerny <nicolas.aerny@gmail.com>
2024-05-23 14:30:14 +0200

    base Vuejs setup

---
+++ laravel/resources/js/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+</script>
+
+<template>
+    <div>
+        <h1>Vaud Cul' From vue</h1>
+    </div>
+</template>
+
+<style scoped>
+/* Your component's CSS styles go here */
+</style>
---
+++ laravel/resources/views/home.blade.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Vaud Cul'</title>
+        @vite(['resources/js/app.js'])
+
+    </head>
+    <body>
+        <div id="app">
+            <h1>Vaud Cul'</h1>
+            <app></app>
+        </div>
+    </body>
+</html>
