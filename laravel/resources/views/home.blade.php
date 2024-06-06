3fac95b99e7fe3673b5a5ee88bb45fdf7b506201
ClementKunzi <clement.kunzi@ikmail.com>
2024-06-06 10:33:10 +0200

    modification des configurations pour le deploy

--- laravel/resources/views/home.blade.php
+++ laravel/resources/views/home.blade.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">        
 
-        <title>Vaud Cul'</title>
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
-    </head>
-    <body>        
-        <div id="app" class="layout min-h-screen flex flex-col font-sans bg-tv-eggshell">
-            <h1>Vaud Cul'</h1>
-            <app></app>
-        </div>
-    </body>
+    <title>Vaud Cul'</title>
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+</head>
+
+<body>
+    <div id="app" class="layout min-h-screen flex flex-col font-sans bg-tv-eggshell">
+        <h1>Vaud Cul'</h1>
+        <app></app>
+    </div>
+</body>
+
 </html>
--- laravel/tailwind.config.js
+++ laravel/tailwind.config.js
@@ -1,42 +1,28 @@
 /** @type {import('tailwindcss').Config} */
-export default {
-  content: [
-    "./resources/**/*.blade.php",
-    "./resources/**/*.js",
-    "./resources/**/*.vue",
-  ],
-  options: {
-    safelist: ['.router-link-active'],
-  },
-  theme: {
-    extend: {
-      colors: {
-        'tv-wine': '#754043',
-        'tv-beige': '#FAF0CA',
-        'tv-eggshell': '#F5f5f5',
-      },
-      boxShadow: {
-        // 'tv': '0px 11px 3px rgba(0, 0, 0, 0), 0px 7px 3px, rgba(0, 0, 0, 0.01), 0px 4px 2px rgba(0, 0, 0, 0.05), 0px 2px 2px rgba(0, 0, 0, 0.09), 0px 0px 1px rgba(0, 0, 0, 0.1)',
-        'tv': '0px 11px 3px rgba(0, 0, 0, 0), 0px 7px 3px rgba(0, 0, 0, 0.01), 0px 4px 2px rgba(0, 0, 0, 0.05), 0px 2px 2px rgba(0, 0, 0, 0.09), 0px 0px 1px rgba(0, 0, 0, 0.1)',
-      
-      },
+const config = {
+    content: [
+        "./resources/**/*.blade.php",
+        "./resources/**/*.js",
+        "./resources/**/*.vue",
+    ],
+    theme: {
+        extend: {
+            colors: {
+                'tv-wine': '#754043',
+                'tv-beige': '#FAF0CA',
+                'tv-eggshell': '#F5f5f5',
+            },
+            boxShadow: {
+                'tv': '0px 11px 3px rgba(0, 0, 0, 0), 0px 7px 3px rgba(0, 0, 0, 0.01), 0px 4px 2px rgba(0, 0, 0, 0.05), 0px 2px 2px rgba(0, 0, 0, 0.09), 0px 0px 1px rgba(0, 0, 0, 0.1)',
+            },
+            fontFamily: {
+                'sans': ['Bilo', 'Roboto', 'ui-sans-serif', 'System UI'],
+                'serif': ['"Made Mountain"', 'ui-serif', 'Georgia',],
+            },
+        },
     },
-    fontFamily: {
-      'sans': ['bilo', 'roboto', 'ui-sans-serif', 'system-ui'],
-      'serif': ['"Made Mountain"', 'ui-serif', 'Georgia', ],      
-    }
-  },
-  purge: {    
-    options: {
-  
-    safelist: ['.router-link-active',
-    '.leaflet-routing-container',
-    'leaflet-control-geocoder',
-    '.leaflet-control-geocoder-icon',
-    'leaflet-marker-pane'],
-    },
-  },
-  plugins: [],
-}
+    safelist: ['.router-link-active', '.leaflet-routing-container', '.leaflet-control-geocoder', '.leaflet-control-geocoder-icon', '.leaflet-marker-pane'],
+    plugins: [],
+};
 
-
+module.exports = config;
